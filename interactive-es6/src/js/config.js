eed8602e51a5b92075858d09988342fbc835ed31
Niko Kommenda <nkommenda@hotmail.com>
2016-06-09 17:22:38 +0100

    force graph

--- interactive-es6/src/js/config.js
+++ interactive-es6/src/js/config.js
@@ -22,12 +22,14 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
+    "d3": "npm:d3@3.5.17",
     "guardian/iframe-messenger": "github:guardian/iframe-messenger@master",
     "json": "github:systemjs/plugin-json@0.1.2",
+    "lodash": "npm:lodash@4.13.1",
     "reqwest": "github:ded/reqwest@1.1.5",
     "text": "github:systemjs/plugin-text@0.0.2",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.4.0"
+      "assert": "npm:assert@1.4.1"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
@@ -41,18 +43,14 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:assert@1.4.0": {
+    "npm:assert@1.4.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "buffer-shims": "npm:buffer-shims@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:buffer-shims@1.0.0": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
     "npm:buffer@3.6.0": {
       "base64-js": "npm:base64-js@0.0.8",
@@ -70,6 +68,10 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@4.13.1": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
--- interactive-es6/src/js/main.js
+++ interactive-es6/src/js/main.js
@@ -1,21 +1,110 @@
 import reqwest from 'reqwest'
 import mainHTML from './text/main.html!text'
 import share from './lib/share'
+import d3 from 'd3'
+import _ from 'lodash'
+import data from './interactions.json!'
 
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 
+let testData = {
+  vertices : [
+    { user : 'a' },
+    { user : 'b' },
+    { user : 'c' }
+  ],
+  edges : [
+    { 'source' : 0, 'target' : 1, 'value' : 10 },
+    { 'source' : 1, 'target' : 2, 'value' : 9 },
+    { 'source' : 0, 'target' : 2, 'value' : 5 }
+  ]
+}
+
+console.log(data)
+
+let exp = 2
+
+let min = d3.min(data.edges.map(e => e.value))
+let max = d3.max(data.edges.map(e => e.value))
+console.log(min)
+console.log(max)
+
+let linkScale = d3.scale.linear()
+  .domain([0, d3.max(data.edges.map(e => e.value))])
+  .range([0, 80])
+
+let opacityScale = d3.scale.linear()
+  .domain([0, d3.max(data.edges.map(e => e.value))])
+  .range([0.05, 0.6])
+
 export function init(el, context, config, mediator) {
-    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
 
-    reqwest({
-        url: 'http://ip.jsontest.com/',
-        type: 'json',
-        crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
-    });
+  el.innerHTML = ''
 
-    [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
-        var network = shareEl.getAttribute('data-network');
-        shareEl.addEventListener('click',() => shareFn(network));
-    });
+  let width = 800, height = 800
+
+  let force = d3.layout.force()
+    .charge(-20)
+    .linkDistance((l, i) => {
+      return linkScale(max - l.value)
+    })
+    .linkStrength(1)
+    // .linkStrength((link, i) => {
+    //
+    //   let val = Math.pow(exp, -link.value)
+    //   //console.log(linkScale(link.value))
+    //   return linkScale(val)
+    // })
+    .size([width, height])
+
+  force.on('tick', () => {
+     link
+       .attr('x1', d => d.source.x)
+       .attr('y1', d => d.source.y)
+       .attr('x2', d => d.target.x)
+       .attr('y2', d => d.target.y)
+
+    vertex
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+    })
+
+  force
+    .nodes(data.vertices)
+    .links(data.edges)
+    .start()
+
+
+  let svg = d3.select(el)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+  let link = svg.selectAll('.edge')
+    .data(data.edges)
+    .enter()
+    .append('line')
+    .style('stroke-width', e => {
+      return 2//opacityScale(e.value)*10
+    })
+    .style('stroke-opacity', e => {
+      return opacityScale(e.value)
+    })
+    .style('stroke', e => {
+      if(e.value === min){
+        return 'red'
+      }
+      else if(e.value === max){
+        return 'green'
+      }
+      return 'black'
+    })
+
+  let vertex = svg.selectAll('.vertex')
+    .data(data.vertices)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .style('fill', '#448')
+    .call(force.drag)
 }
